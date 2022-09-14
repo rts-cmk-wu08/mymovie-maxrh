4388586f80ab5dfc279e9a5f11881ae7168b73c4
Max Hargreave <max@monsun.dk>
2022-09-14 13:46:28 +0200

    Popular section fetch

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	let items = document.querySelector(".showing-list")
-
+	let showingItems = document.querySelector(".showing-list")
+	let popularItems = document.querySelector(".popular-list")
 
 	fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=e566d191afaba5f363a396fd59c36a61')
   		.then((response) => response.json())
@@ -20,17 +20,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 				
-				items.append(item)
-
-
-				console.log(result)
-
-
+				showingItems.append(item)
 			})
 		
 
 
 			console.log(data)
+
+		})
+
+
+		fetch('https://api.themoviedb.org/3/movie/popular?api_key=e566d191afaba5f363a396fd59c36a61')
+  		.then((response) => response.json())
+  		.then((data) => {
+
+			data.results.forEach(result => {
+				
+				let item = document.createElement("li")
+				item.classList.add("item")
+
+				item.innerHTML = `
+					<h1>${result.original_title}</h1>
+					`
+
+
+				
+				popularItems.append(item)
+			})
+		
 
 		})
 
